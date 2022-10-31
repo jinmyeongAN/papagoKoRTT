5441bd476bed05cf5b68f13bed9964ab08c2a988
jinmyeongAN <jinmyeong.an@gmail.com>
2022-10-31 17:31:31 +0900

    [main.py] modify main function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from utils.PapagoKoRTT import PapagoKoRTT
 
 class Sentence(BaseModel):
     setence: str
@@ -12,6 +13,7 @@ app = FastAPI()
 async def round_trip_trans(input_sentence: Sentence):
     sentence_dict = input_sentence.dict()
     if input_sentence.setence:
-        output = papago_KoRTT(input_sentence.setence)
+        papagoKoRTT = PapagoKoRTT(input_sentence.setence)
+        output = papagoKoRTT.roundTripTranslation()
         sentence_dict.update({"sentence": output})
     return sentence_dict
