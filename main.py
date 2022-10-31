f6e4cd63dd40e8fa4d5b9d7cb8671e5eff304287
jinmyeongAN <jinmyeong.an@gmail.com>
2022-10-31 15:56:31 +0900

    [Setting] post request setting using FastAPI

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Sentence(BaseModel):
+    setence: str
 
 app = FastAPI()
 
 
-@app.get("/papago")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/papago/")
+async def round_trip_trans(input_sentence: Sentence):
+    sentence_dict = input_sentence.dict()
+    if input_sentence.setence:
+        output = papago_KoRTT(input_sentence.setence)
+        sentence_dict.update({"sentence": output})
+    return sentence_dict
